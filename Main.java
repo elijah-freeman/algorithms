c1f889c3f8127459ea0172518dad379fe44f222e
Elijah Freeman <elijah@elijahfreeman.com>
2021-06-15 23:22:30 -0700

    Fixed some method headers for bubble sorting a linked list

--- Main.java
+++ Main.java
@@ -27,25 +27,33 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		//Directions: Rename the below file name to sort elements in that txt file.
-	//	String fileName = "test/random10000.txt";
-	//	PrintStream output = null;
-	//	try {
-	//		output = new PrintStream(new File("test/Output.txt"));
-	//	} catch (FileNotFoundException e) {
-	//		System.out.println("ERROR " + e);
-	//		System.exit(1);
-	//	}
-	//	//displays the sorted list to the console.
-	//	//writes appropriate data to text file.
-	//	resultBubbleSort(fileName, output);
-	//	resultShellSort(fileName, output);
-	//	
+		String fileName = "test/random10000.txt";
+		PrintStream output = null;
+		try {
+			output = new PrintStream(new File("test/Output.txt"));
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR " + e);
+			System.exit(1);
+		}
+		//displays the sorted list to the console.
+		//writes appropriate data to text file.
+		//resultBubbleSort(fileName, output);
+		//resultShellSort(fileName, output);
+		
 	
-		int[] array = {3, 2, 7, 1, 9, 5, 8, 4};
-		System.out.printf("Input Array: %s\n", Arrays.toString(array));
+		//int[] array = {3, 2, 7, 1, 9, 5, 8, 4};
+		//System.out.printf("Input Array: %s\n", Arrays.toString(array));
+		//Sort sort = new Sort();
+		//sort.bubbleSort(array);
+		//System.out.printf("Output Array: %s\n", Arrays.toString(array));
+		
 		Sort sort = new Sort();
-		sort.bubbleSort(array);
-		System.out.printf("Output Array: %s\n", Arrays.toString(array));
+		LinkedNode<Integer> head = readValues(fileName);
+		displayList(head);
+		//TODO fix bubble sort for linked list. Losing most of the values somewhere.
+		sort.bubbleSort(head);
+		displayList(head);
+
 	}
 
 	/**
@@ -87,45 +95,45 @@ public class Main {
 		}
 	}
 
-	public static void resultShellSort(String fileName, PrintStream output) {
-		System.out.println();
-		System.out.println();
-		LinkedNode<Integer> head = readValues(fileName);
-		System.out.println();
-		System.out.println("Unsorted List:");
-		displayList(head);
-		output.println("First 20 passes: shellsort  " + fileName);
-		output.println("gap       pass      cmp            exch");
-		output.println("--------------------------------------");
-		Sort sorting = new Sort(output);
-		head = sorting.shellSort(head);
-		System.out.println();
-		System.out.println();
-		System.out.println("Shelled Sorted list: ");
-		displayList(head);
-		for (int i = 0; i < 10; i++) {
-			output.println();
-		}
-	}
+	//public static void resultShellSort(String fileName, PrintStream output) {
+	//	System.out.println();
+	//	System.out.println();
+	//	LinkedNode<Integer> head = readValues(fileName);
+	//	System.out.println();
+	//	System.out.println("Unsorted List:");
+	//	displayList(head);
+	//	output.println("First 20 passes: shellsort  " + fileName);
+	//	output.println("gap       pass      cmp            exch");
+	//	output.println("--------------------------------------");
+	//	Sort sorting = new Sort(output);
+	//	head = sorting.shellSort(head);
+	//	System.out.println();
+	//	System.out.println();
+	//	System.out.println("Shelled Sorted list: ");
+	//	displayList(head);
+	//	for (int i = 0; i < 10; i++) {
+	//		output.println();
+	//	}
+	//}
 
-	public static void resultBubbleSort(String fileName, PrintStream output) {
-		System.out.println();
-		System.out.println();
-		LinkedNode<Integer> head = readValues(fileName);
-		System.out.println();
-		System.out.println("Unsorted List:");
-		displayList(head);
-		output.println("First 20 passes: bubbleSort  " + fileName);
-		output.println("         pass        cmp         exch");
-		output.println("---------------------------------------");
-		Sort sorting = new Sort(output);
-		head = sorting.bubbleSort(head);
-		System.out.println();
-		System.out.println();
-		System.out.println("Bubbled Sorted List");
-		displayList(head);
-		for (int i = 0; i < 10; i++) {
-			output.println();
-		}
-	}
+	//public static void resultBubbleSort(String fileName, PrintStream output) {
+	//	System.out.println();
+	//	System.out.println();
+	//	LinkedNode<Integer> head = readValues(fileName);
+	//	System.out.println();
+	//	System.out.println("Unsorted List:");
+	//	displayList(head);
+	//	output.println("First 20 passes: bubbleSort  " + fileName);
+	//	output.println("         pass        cmp         exch");
+	//	output.println("---------------------------------------");
+	//	Sort sorting = new Sort(output);
+	//	head = sorting.bubbleSort(head);
+	//	System.out.println();
+	//	System.out.println();
+	//	System.out.println("Bubbled Sorted List");
+	//	displayList(head);
+	//	for (int i = 0; i < 10; i++) {
+	//		output.println();
+	//	}
+	//}
 }
--- Sort.java
+++ Sort.java
@@ -45,185 +45,214 @@ public class Sort {
 		}
 	}
 
-	public LinkedNode<Integer> bubbleSort(LinkedNode<Integer> first) {
-		LinkedNode<Integer> currentOuter = first;
-		LinkedNode<Integer> currentInner = first;
-		LinkedNode<Integer> previous = null;
-		LinkedNode<Integer> next = currentInner.getNext();
-		LinkedNode<Integer> temp;
-		int comparison;
-		int exchange;
-		int numPass = 0;
-		int totalPasses = 0;
-		int totalComparisons = 0;
-		int totalExchanges = 0;
-		int numberPrints = 0;
-		sortType = true;
-		boolean swapCheck = true;
-
-
-		while(swapCheck) {
-			exchange = 0;
-			comparison = 0;
-			swapCheck = false;
-
-			while(next != null) {
-				if (currentInner.getElement() > next.getElement()) {
-					temp = next.getNext();
-					exchange++;
-					if (previous != null) {
-						previous.setNext(next);
-					} else {
-						first = next;
-					}
-					next.setNext(currentInner);
-					currentInner.setNext(temp);
-					previous = next;
-					next = currentInner.getNext();
-					swapCheck = true;
+	/**
+	 * In place sorting method using the bubble sort algorithm.
+	 *
+	 * @param  head  the pointer the the first element of a linked list.
+	 */
+	public void bubbleSort(LinkedNode<Integer> head) {
+		LinkedNode<Integer> temp = head;
+		while (temp.getNext() != null) {
+			LinkedNode<Integer> previous = temp;
+			LinkedNode<Integer> current = temp.getNext();
+			LinkedNode<Integer> next = temp.getNext().getNext();
+			while (current.getNext() != null) {
+				if (current.getElement() > next.getElement()) {
+					previous.setNext(next); 
+					current.setNext(next.getNext());
+					next = current.getNext();
 				} else {
-					previous = currentInner;
-					currentInner = next;
+					previous = current;
+					current = next;
 					next = next.getNext();
 				}
-				comparison++;
 			}
-			currentInner = first;
-			previous = null;
-			next = currentInner.getNext();
-			numPass++;
-			totalPasses++;
-			totalComparisons+=comparison;
-			totalExchanges+=exchange;
-
-			if (!callFromShell) {
-				if (exchange != 0 && numberPrints < 20) {
-					writeOutput(first, 0, numPass, comparison, 
-							exchange, bubbleFlag, sortType);
-					numberPrints++;
-				}
-			}
+			temp = temp.getNext();
 		}
-		if (!callFromShell) {
-			bubbleFlag = false;
-			output.println("-----------------------------------------");
-			output.println("Total:     pass        cmp         exch");
-			writeOutput(first, 0, totalPasses, totalComparisons, 
-					totalExchanges, bubbleFlag, sortType);
-		}
-		return first;
 	}
 
 
-	public LinkedNode<Integer> shellSort(LinkedNode<Integer> first) {
 
-		LinkedNode<Integer> current;
-		LinkedNode<Integer> next;
-		LinkedNode<Integer> currentPrevious;
-		LinkedNode<Integer> nextPrevious;
-		LinkedNode<Integer> listCounter = first;
-
-		sortType = false;
-		boolean shellFlag = false;
-		boolean swapCheck = true;
-		int comparison = 0;
-		int exchange = 0;
-		int numPass = 0;
-		int totalPasses = 0;
-		int totalComparisons = 0;
-		int totalExchanges = 0;
-		int listLength = 0;
-		int k = 0;
-		int gap = 0;
-		int counter = 0;
-		int numberPrints = 0;
-
-		gap = (int) ((Math.pow(3, k) - 1) / 2);
-
-		while (listCounter != null) {
-			listLength++;
-			listCounter = listCounter.getNext();
-		}
+	
+	///public LinkedNode<Integer> bubbleSort(LinkedNode<Integer> first) {
+	///	LinkedNode<Integer> currentOuter = first;
+	///	LinkedNode<Integer> currentInner = first;
+	///	LinkedNode<Integer> previous = null;
+	///	LinkedNode<Integer> next = currentInner.getNext();
+	///	LinkedNode<Integer> temp;
+	///	int comparison;
+	///	int exchange;
+	///	int numPass = 0;
+	///	int totalPasses = 0;
+	///	int totalComparisons = 0;
+	///	int totalExchanges = 0;
+	///	int numberPrints = 0;
+	///	sortType = true;
+	///	boolean swapCheck = true;
 
 
-		int tempGap = 0;
-		for (; gap < listLength; gap = (int) ((Math.pow(3, k) -1)/2)) {
-			++k;
-			tempGap = gap;
-		}
-		k--;
-		gap = tempGap;
+	///	while(swapCheck) {
+	///		exchange = 0;
+	///		comparison = 0;
+	///		swapCheck = false;
+
+	///		while(next != null) {
+	///			if (currentInner.getElement() > next.getElement()) {
+	///				temp = next.getNext();
+	///				exchange++;
+	///				if (previous != null) {
+	///					previous.setNext(next);
+	///				} else {
+	///					first = next;
+	///				}
+	///				next.setNext(currentInner);
+	///				currentInner.setNext(temp);
+	///				previous = next;
+	///				next = currentInner.getNext();
+	///				swapCheck = true;
+	///			} else {
+	///				previous = currentInner;
+	///				currentInner = next;
+	///				next = next.getNext();
+	///			}
+	///			comparison++;
+	///		}
+	///		currentInner = first;
+	///		previous = null;
+	///		next = currentInner.getNext();
+	///		numPass++;
+	///		totalPasses++;
+	///		totalComparisons+=comparison;
+	///		totalExchanges+=exchange;
+
+	///		if (!callFromShell) {
+	///			if (exchange != 0 && numberPrints < 20) {
+	///				writeOutput(first, 0, numPass, comparison, 
+	///						exchange, bubbleFlag, sortType);
+	///				numberPrints++;
+	///			}
+	///		}
+	///	}
+	///	if (!callFromShell) {
+	///		bubbleFlag = false;
+	///		output.println("-----------------------------------------");
+	///		output.println("Total:     pass        cmp         exch");
+	///		writeOutput(first, 0, totalPasses, totalComparisons, 
+	///				totalExchanges, bubbleFlag, sortType);
+	///	}
+	///	return first;
+	///}
 
 
-		while (gap > 1) {
-			current = first.getNext();
-			currentPrevious = first;
-			next = first.getNext();
-			nextPrevious = first;
-			swapCheck =  true;
+	//public LinkedNode<Integer> shellSort(LinkedNode<Integer> first) {
 
-			while (counter < gap) {
-				counter++;
-				nextPrevious = next;
-				next = next.getNext();
-			}
-			counter = 0;
+	//	LinkedNode<Integer> current;
+	//	LinkedNode<Integer> next;
+	//	LinkedNode<Integer> currentPrevious;
+	//	LinkedNode<Integer> nextPrevious;
+	//	LinkedNode<Integer> listCounter = first;
 
-			while(swapCheck) {
-				swapCheck = false;
-				while (next != null) {
-					if (current.getElement() > next.getElement()) {
-						nextPrevious.setNext(next.getNext());
-						next.setNext(current.getNext());
-						currentPrevious.setNext(next);
-						current.setNext(nextPrevious.getNext());
-						nextPrevious.setNext(current);
-						exchange++;
-						swapCheck =  true;
-					}
+	//	sortType = false;
+	//	boolean shellFlag = false;
+	//	boolean swapCheck = true;
+	//	int comparison = 0;
+	//	int exchange = 0;
+	//	int numPass = 0;
+	//	int totalPasses = 0;
+	//	int totalComparisons = 0;
+	//	int totalExchanges = 0;
+	//	int listLength = 0;
+	//	int k = 0;
+	//	int gap = 0;
+	//	int counter = 0;
+	//	int numberPrints = 0;
 
-					currentPrevious = currentPrevious.getNext();
-					nextPrevious = nextPrevious.getNext();
-					current = currentPrevious.getNext();
-					next = nextPrevious.getNext();
-					comparison++;
-				}
-				current = first.getNext();
-				currentPrevious = first;
-				next = first.getNext();
-				nextPrevious = first;
-				numPass++;
-				while (counter < gap) {
-					counter++;
-					nextPrevious = next;
-					next = next.getNext();
-				}
-				counter = 0;
-			}
+	//	gap = (int) ((Math.pow(3, k) - 1) / 2);
 
-			numPass -= 1;
-			if (exchange != 0 && numberPrints < 20) {
-				writeOutput(first, gap, numPass, comparison, 
-						exchange, shellFlag, sortType);
-				numberPrints++;
-			}
-			gap = (int) ((Math.pow(3, --k) - 1) / 2);
-			totalPasses += numPass;
-			totalComparisons += comparison;
-			totalExchanges += exchange;
-		}
-		if (gap == 1) {
-			callFromShell = true;
-			first = bubbleSort(first);
-			writeOutput(first, gap , numPass, comparison, exchange, shellFlag, sortType);
-			callFromShell = false;
-		}
-		output.println("------------------------------------------");
-		output.println("Total:    pass          cmp          exch");
-		writeOutput(first, gap, totalPasses, totalComparisons, totalExchanges, 
-				shellFlag, sortType);
-		return first;
-	}
+	//	while (listCounter != null) {
+	//		listLength++;
+	//		listCounter = listCounter.getNext();
+	//	}
+
+
+	//	int tempGap = 0;
+	//	for (; gap < listLength; gap = (int) ((Math.pow(3, k) -1)/2)) {
+	//		++k;
+	//		tempGap = gap;
+	//	}
+	//	k--;
+	//	gap = tempGap;
+
+
+	//	while (gap > 1) {
+	//		current = first.getNext();
+	//		currentPrevious = first;
+	//		next = first.getNext();
+	//		nextPrevious = first;
+	//		swapCheck =  true;
+
+	//		while (counter < gap) {
+	//			counter++;
+	//			nextPrevious = next;
+	//			next = next.getNext();
+	//		}
+	//		counter = 0;
+
+	//		while(swapCheck) {
+	//			swapCheck = false;
+	//			while (next != null) {
+	//				if (current.getElement() > next.getElement()) {
+	//					nextPrevious.setNext(next.getNext());
+	//					next.setNext(current.getNext());
+	//					currentPrevious.setNext(next);
+	//					current.setNext(nextPrevious.getNext());
+	//					nextPrevious.setNext(current);
+	//					exchange++;
+	//					swapCheck =  true;
+	//				}
+
+	//				currentPrevious = currentPrevious.getNext();
+	//				nextPrevious = nextPrevious.getNext();
+	//				current = currentPrevious.getNext();
+	//				next = nextPrevious.getNext();
+	//				comparison++;
+	//			}
+	//			current = first.getNext();
+	//			currentPrevious = first;
+	//			next = first.getNext();
+	//			nextPrevious = first;
+	//			numPass++;
+	//			while (counter < gap) {
+	//				counter++;
+	//				nextPrevious = next;
+	//				next = next.getNext();
+	//			}
+	//			counter = 0;
+	//		}
+
+	//		numPass -= 1;
+	//		if (exchange != 0 && numberPrints < 20) {
+	//			writeOutput(first, gap, numPass, comparison, 
+	//					exchange, shellFlag, sortType);
+	//			numberPrints++;
+	//		}
+	//		gap = (int) ((Math.pow(3, --k) - 1) / 2);
+	//		totalPasses += numPass;
+	//		totalComparisons += comparison;
+	//		totalExchanges += exchange;
+	//	}
+	//	if (gap == 1) {
+	//		callFromShell = true;
+	//		first = bubbleSort(first);
+	//		writeOutput(first, gap , numPass, comparison, exchange, shellFlag, sortType);
+	//		callFromShell = false;
+	//	}
+	//	output.println("------------------------------------------");
+	//	output.println("Total:    pass          cmp          exch");
+	//	writeOutput(first, gap, totalPasses, totalComparisons, totalExchanges, 
+	//			shellFlag, sortType);
+	//	return first;
+	//}
 
 
 	private void writeOutput(LinkedNode<Integer> head, int gap, int thePass, 
